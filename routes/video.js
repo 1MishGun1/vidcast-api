0fbf1c49076128ba114dd53caf7bb6e35f24443e
1MishGun1 <mikhail.kudrov@gmail.com>
2025-02-17 20:00:03 +0300

    add all videos and one video routes

--- routes/video.js
+++ routes/video.js
@@ -1,7 +1,18 @@
 const videoRouter = require("express").Router();
-const createVideo = require("../controllers/VideoController");
+const {
+  createVideo,
+  getAllVideos,
+  getOneVideo,
+} = require("../controllers/VideoController");
 const checkMe = require("../middlewares/checkMe");
 
+//! Create a new video
 videoRouter.post("/videos", checkMe, createVideo);
+
+//! Get all videos
+videoRouter.get("/videos", getAllVideos);
+
+//! Get all videos
+videoRouter.get("/videos/:id", getOneVideo);
 
 module.exports = videoRouter;
