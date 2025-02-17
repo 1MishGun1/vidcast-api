e5283e023d53317e90aaa6e05ed926f51259d186
1MishGun1 <mikhail.kudrov@gmail.com>
2025-02-17 19:41:16 +0300

    add video, upload routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const mainRouter = require("express").Router();
 
 const userRouter = require("./user");
+const videoRouter = require("./video");
+const uploadRouter = require("./upload");
 
-mainRouter.use(userRouter);
+mainRouter.use(userRouter, videoRouter, uploadRouter);
 
 module.exports = mainRouter;
