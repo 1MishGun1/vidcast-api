56ae5eeae6a4dec743f3fd97898ce8200639bda7
1MishGun1 <mikhail.kudrov@gmail.com>
2025-02-17 21:12:04 +0300

    add main directory

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const connectToDatabase = require("./db/connect");
 
 const mainRouter = require("./routes/index");
+const upload = require("./controllers/UploadController");
 
 const app = express();
 const PORT = 3333;
@@ -9,6 +10,7 @@ const PORT = 3333;
 connectToDatabase();
 
 app.use(express.json());
+app.use("/uploads", express.static("uploads"));
 app.use(mainRouter);
 
 app.get("/", (req, res) => {
