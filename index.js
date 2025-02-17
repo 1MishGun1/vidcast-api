d97e4873674099343f60a7f265988dc431d123d7
1MishGun1 <mikhail.kudrov@gmail.com>
2025-02-17 16:07:40 +0300

    add .json() middleware

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const connectToDatabase = require("./db/connect");
 
+const mainRouter = require("./routes/index");
+
 const app = express();
 const PORT = 3333;
 
 connectToDatabase();
+
+app.use(express.json());
+app.use(mainRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
