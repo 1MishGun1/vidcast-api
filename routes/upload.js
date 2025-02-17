9084becb3e842a4d1f2ae1ef30111a25bf3a7177
1MishGun1 <mikhail.kudrov@gmail.com>
2025-02-17 21:13:00 +0300

    add upload files routes

--- routes/upload.js
+++ routes/upload.js
@@ -1,3 +1,53 @@
 const uploadRouter = require("express").Router();
+const checkMe = require("../middlewares/checkMe");
+const upload = require("../controllers/UploadController");
+
+//! Upload video
+uploadRouter.post(
+  "/uploads/videos",
+  checkMe,
+  upload.single("video"),
+  (req, res) => {
+    res.json({
+      url: `/uploads/videos/${req.file.filename}`,
+    });
+  }
+);
+
+//! Upload cover for video
+uploadRouter.post(
+  "/uploads/coversVideos",
+  checkMe,
+  upload.single("imgVideo"),
+  (req, res) => {
+    res.json({
+      url: `/uploads/coversVideos/${req.file.filename}`,
+    });
+  }
+);
+
+//! Upload cover for user
+uploadRouter.post(
+  "/uploads/coversUsers",
+  checkMe,
+  upload.single("imgUser"),
+  (req, res) => {
+    res.json({
+      url: `/uploads/coversUsers/${req.file.filename}`,
+    });
+  }
+);
+
+//! Upload avatars user
+uploadRouter.post(
+  "/uploads/avatars",
+  checkMe,
+  upload.single("avatar"),
+  (req, res) => {
+    res.json({
+      url: `/uploads/avatars/${req.file.filename}`,
+    });
+  }
+);
 
 module.exports = uploadRouter;
