da69916dac7bbf8af02265a051ddc21491cab283
1MishGun1 <mikhail.kudrov@gmail.com>
2025-02-17 14:48:52 +0300

    create Video model

---
+++ models/Video.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const VideoSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      require: true,
+    },
+    description: {
+      type: String,
+      require: true,
+    },
+    tags: {
+      type: Array,
+      default: [],
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      require: true,
+    },
+    cover: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Video", VideoSchema);
