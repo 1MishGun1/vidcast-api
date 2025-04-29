93ecca9054541b57e04d551505d3e4618d1defe3
1MishGun1 <mikhail.kudrov@gmail.com>
2025-04-29 16:25:25 +0300

    create playlist model

---
+++ models/Playlist.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const PlaylistSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  videos: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Video",
+  }],
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  isVisible: {
+    type: Boolean,
+    default: true,
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model("Playlist", PlaylistSchema);
