dbfd045d9cd79a7f5d85439052b2b89ded4007cf
1MishGun1 <mikhail.kudrov@gmail.com>
2025-06-02 18:04:40 +0300

    replace routes

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,24 @@
 const userRouter = require("express").Router();
-const { register, login, getMe } = require("../controllers/UserController");
+const {
+  register,
+  login,
+  getMe,
+  getAllUsers,
+  getOneUser,
+  toggleSubscription,
+  checkSubscription,
+  getSubscribersCount,
+} = require("../controllers/UserController");
 const { registerValidation, loginValidation } = require("../validations/auth");
 const checkMe = require("../middlewares/checkMe");
 
 userRouter.post("/register", registerValidation, register);
 userRouter.post("/login", loginValidation, login);
 userRouter.get("/me", checkMe, getMe);
+userRouter.get("/users", getAllUsers);
+userRouter.get("/users/:id", getOneUser);
+userRouter.post("/subscribe/:id", checkMe, toggleSubscription);
+userRouter.get("/is-subscribed/:id", checkMe, checkSubscription);
+userRouter.get("/subscribers-count/:id", getSubscribersCount);
 
 module.exports = userRouter;
