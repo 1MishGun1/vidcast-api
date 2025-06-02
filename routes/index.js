ade98687e38da85ea77a04be1ccaeed1dc19637b
1MishGun1 <mikhail.kudrov@gmail.com>
2025-06-02 18:08:01 +0300

    add reactionRouter

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,9 @@ const userRouter = require("./user");
 const videoRouter = require("./video");
 const playlistRouter = require("./playlist");
 const uploadRouter = require("./upload");
+const reactionRouter = require("./reaction");
 
 mainRouter.use(userRouter, videoRouter, playlistRouter, uploadRouter);
+mainRouter.use("/reactions", reactionRouter);
 
 module.exports = mainRouter;
