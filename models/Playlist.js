c96c37bfbd998d0b647a14981b43c297c474fc3b
1MishGun1 <mikhail.kudrov@gmail.com>
2025-06-03 07:11:44 +0300

    auto format

--- models/Playlist.js
+++ models/Playlist.js
@@ -1,26 +1,31 @@
 const mongoose = require("mongoose");
 
-const PlaylistSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const PlaylistSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    videos: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    isVisible: {
+      type: Boolean,
+      default: true,
+    },
   },
-  description: {
-    type: String,
-  },
-  videos: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Video",
-  }],
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  isVisible: {
-    type: Boolean,
-    default: true,
-  }
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Playlist", PlaylistSchema);
