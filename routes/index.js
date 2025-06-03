a3da5bdbdc8065b670e82e05241b145924901091
1MishGun1 <mikhail.kudrov@gmail.com>
2025-06-03 07:12:06 +0300

    import commentRouter

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,10 @@ const videoRouter = require("./video");
 const playlistRouter = require("./playlist");
 const uploadRouter = require("./upload");
 const reactionRouter = require("./reaction");
+const commentRouter = require("./comment");
 
 mainRouter.use(userRouter, videoRouter, playlistRouter, uploadRouter);
 mainRouter.use("/reactions", reactionRouter);
+mainRouter.use("/comments", commentRouter);
 
 module.exports = mainRouter;
