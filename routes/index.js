17dc8127a8e063b7d03fe44d218d3cee05d3ee7a
1MishGun1 <mikhail.kudrov@gmail.com>
2025-06-11 18:07:49 +0300

    add searchRouter

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,11 @@ const playlistRouter = require("./playlist");
 const uploadRouter = require("./upload");
 const reactionRouter = require("./reaction");
 const commentRouter = require("./comment");
+const searchRouter = require("./search");
 
 mainRouter.use(userRouter, videoRouter, playlistRouter, uploadRouter);
 mainRouter.use("/reactions", reactionRouter);
 mainRouter.use("/comments", commentRouter);
+mainRouter.use("/search", searchRouter);
 
 module.exports = mainRouter;
