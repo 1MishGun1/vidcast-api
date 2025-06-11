2edabadd4dfb0137073b86a64de605f33fe057c9
1MishGun1 <mikhail.kudrov@gmail.com>
2025-06-11 18:07:57 +0300

    create startServer

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const connectToDatabase = require("./db/connect");
 const cors = require("cors");
 
 const mainRouter = require("./routes/index");
+const { initEs, indexVideo } = require("./elastic-search/elastic");
+const Video = require("./models/Video");
 
 const app = express();
 const PORT = 3333;
 
 connectToDatabase();
+initEs();
 
 app.use(express.json());
 app.use(cors());
@@ -18,6 +21,19 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(PORT, () => {
-  console.log(`App running in http://localhost:${PORT}`);
-});
+const startServer = async () => {
+  try {
+    const videos = await Video.find();
+    for (const video of videos) {
+      await indexVideo(video);
+    }
+
+    app.listen(PORT, () => {
+      console.log(`App running in http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to start server:", error);
+  }
+};
+
+startServer();
